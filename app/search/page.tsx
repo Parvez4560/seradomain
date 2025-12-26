89f2220a54047ec29cee6c1ebac807958bb5cb8a
Parvez Mia <salafipay@gmail.com>
2025-12-26 16:44:21 +0600

    New changes

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -3,21 +3,21 @@
 import Navbar from "../../components/Navbar";
 import FooterSection from "../../components/FooterSection";
 import SearchContent from "../../components/SearchContent";
+import { Suspense } from "react";
 
 export default function SearchPage() {
   return (
     <div className="font-sans bg-white dark:bg-black min-h-screen flex flex-col">
-      {/* Navbar */}
       <div className="sticky top-0 z-50">
         <Navbar />
       </div>
 
-      {/* Search content */}
       <main className="flex-1 p-6 flex flex-col items-center">
-        <SearchContent />
+        <Suspense fallback={null}>
+          <SearchContent />
+        </Suspense>
       </main>
 
-      {/* Footer */}
       <FooterSection />
     </div>
   );
